95514f132978f9b7f0b98060b0aab1c4f2f50c0d
Feras Alkilani <eng.feras90@gmail.com>
2024-09-21 22:11:44 +0200

    complete the mini profile component

--- src/app/components/Miniprofile.jsx
+++ src/app/components/Miniprofile.jsx
@@ -1,5 +1,37 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
 import React from "react";
 
 export default function Miniprofile() {
-  return <div>Miniprofile</div>;
+  const { data: session } = useSession();
+  return (
+    <div className="flex items-center justify-between mt-14- scroll-ml-10">
+      <img
+        src={session?.user?.image || "/800px-Instagram_logo_2016.webp"}
+        alt="user-profile-pic or instagram logo"
+        className="w-16 h-16 rounded-full border p-[2px]"
+      />
+      <div className="flex-1 ml-4">
+        <h2 className="font-bold">{session?.user?.username}</h2>
+        <h3 className="text-sm text-gray-400">Welcome To Instagram</h3>
+      </div>
+      {session ? (
+        <button
+          onClick={signOut}
+          className="text-sm font-semibold text-blue-500"
+        >
+          Log Out
+        </button>
+      ) : (
+        <button
+          onClick={signIn}
+          className="text-sm font-semibold text-blue-500"
+        >
+          Log In
+        </button>
+      )}
+    </div>
+  );
 }
--- src/app/components/Posts.jsx
+++ src/app/components/Posts.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Posts() {
-  return <div>Postss</div>;
+  return <div>Postssf</div>;
 }
